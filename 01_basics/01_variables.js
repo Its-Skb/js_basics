ede86b44649bedffe88e0546fd630e6479aa70af
Its-Skb <128614625+Its-Skb@users.noreply.github.com>
2026-04-19 17:07:54 +0000

    variables and satatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 1234567890
+let accountEmail = "user@example.com"
+var accountPassword = "123456"
+accountCity = "Patna"
+let accountState;
+console.log(accountId);
+
+accountEmail = "newemail@example.com"
+accountPassword = "1234567890"
+accountCity = "Mumbai"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+"use strict"; // treat all JS code as newer version
+
+
+
+// number = 2 to power of 53
+// bigint = 2 to power of 63
+// string = ""
+// boolean = true/false
+// null = standalone value
+// undefined
+// symbol = unique identifier
+
+// object
+
+console.log(typeof 1234567890)
