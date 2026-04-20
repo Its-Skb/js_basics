8116d27180ebc58e3a99630e6025f968d0ed2a53
Its-Skb <128614625+Its-Skb@users.noreply.github.com>
2026-04-20 09:48:24 +0000

    Conversion of Datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,31 @@
+let score = true
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33ad" => NaN
+// true => 1
+// false => 0
+// "true" => NaN
+// "false" => NaN
+
+let isLoggedIn = "SK"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true
+// 0 => false
+// "SK" => true
+// "" => false
+
+let someNumber = 55
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
