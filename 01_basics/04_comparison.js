54f6fa372c02f76cefb9ad30444c3ca96e913139
Its-Skb <128614625+Its-Skb@users.noreply.github.com>
2026-04-20 12:08:59 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,28 @@
+// console.log(3 > 2);
+// console.log(3 < 2);
+// console.log(3 >= 2);
+// console.log(3 <= 2);
+// console.log(3 == 2);
+// console.log(3 != 2);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined >= 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+
+
+// === checks for value and type
+// == checks for value only
+
+console.log(3 === 3); // true
+console.log(3 === "3"); // false because type is different
+console.log(3 == "3"); // true because value is same and == does type coercion
