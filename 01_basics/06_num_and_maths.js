8a91997d4c1c5797ec0a67276ba980e57a5d9cd8
Its-Skb <128614625+Its-Skb@users.noreply.github.com>
2026-04-22 11:26:33 +0000

    math and num

---
+++ 01_basics/06_num_and_maths.js
@@ -0,0 +1,27 @@
+const score = 400
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.456789
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 100000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// +++++++++++++++++++ Maths ++++++++++++++++++
+
+
+// console.log(Math);
+// console.log(Math.abs(-5));
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.9));
+// console.log(Math.max(1, 2, 3, 4, 5));
+// console.log(Math.min(1, 2, 3, 4, 5));
+
+console.log(Math.random());
